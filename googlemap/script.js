c615a967a32a28a7f2f9b077c41b6cec6e43e9f5
orgoca <orgoca@gmail.com>
2019-01-16 17:20:11 -0500

    temp

--- googlemap/script.js
+++ googlemap/script.js
@@ -16,7 +16,7 @@ function initMap() {
 
 	var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 13,
-		center: new google.maps.LatLng(41.976816, -87.659916),
+		center: new google.maps.LatLng(40.755333, -73.945166),
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	});
 
