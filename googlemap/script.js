34f59c35f99522acbfc3c8b8264e9b74c790da75
orgoca <orgoca@gmail.com>
2019-01-16 18:19:27 -0500

    test

--- googlemap/script.js
+++ googlemap/script.js
@@ -1,37 +1,30 @@
 function initMap() {
 
-	var QU8641 = { 
+	var QU8641 = {
 		info: '<strong>Free - Up to 25 mbs Wi-Fi Service located at Indoor AP - Community Center - Computer Rm</strong>', 
-		Lat: 40.755727, 
-		Long: -73.944583};
+		lat: 40.755727,
+		long: -73.944583
+	};
+
 	var QU8642 = { 
 		info: '<strong>Free - Up to 25 mbs Wi-Fi Service located at Indoor AP - Queens Public Library</strong>', 
 		Lat: 40.755333, 
 		Long: -73.944131};
+
 	var QU8643 = { 
 		info: '<strong>Free - Up to 25 mbs Wi-Fi Service located at Indoor AP - North Management Office</strong>', 
 		Lat: 40.755751, 
 		Long: -73.945166};
-	var QU8644 = { 
-		info: '<strong>Free - Up to 25 mbs Wi-Fi Service located at Indoor AP - North Management Office</strong>', 
-		Lat: 40.755751, 
-		Long: -73.945166};
-	var QU8645 = { 
-		info: '<strong>Free - Up to 25 mbs Wi-Fi Service located at Indoor AP - North Maintenance Area</strong>', 
-		Lat: 40.755751, 
-		Long: -73.945166};
 
 	var locations = [
-    	[QU8641.info, QU8641.lat, QU8641.long, 0],
-		[QU8642.info, QU8642.lat, QU8642.long, 1],
-		[QU8643.info, QU8643.lat, QU8643.long, 2],
-		[QU8644.info, QU8644.lat, QU8644.long, 3],
-		[QU8645.info, QU8645.lat, QU8645.long, 4],
-    		];
+      [QU8641.info, QU8641.lat, QU8641.long, 0],
+      [QU8642.info, QU8642.lat, QU8642.long, 1],
+      [QU8643.info, QU8643.lat, QU8643.long, 2],
+    ];
 
 	var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 13,
-		center: new google.maps.LatLng(40.755333, -73.945166),
+		center: new google.maps.LatLng(40.755751, -73.945166),
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	});
 
